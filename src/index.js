8178b1774509e7cb18e3eb3a0572f7fea7ec0751
vinaykumar912 <mailtovinay912@gmail.com>
2025-08-09 16:48:10 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
@@ -9,6 +10,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start(); // Start the cron job
 app.use(express.json());
 app.use(cors());
 
