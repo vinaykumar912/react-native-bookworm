05eeff249f53a3a5a1de6437d3259b118b0cb2b2
vinaykumar912 <mailtovinay912@gmail.com>
2025-08-23 14:49:55 +0530

    added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -61,6 +61,7 @@ router.post("/register", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt,
       },
     });
   } catch (error) {
